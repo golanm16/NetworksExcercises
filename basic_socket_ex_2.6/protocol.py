ce9d4f55047c2b304377de65d391e97a100f3bb3
Golan <golanm16@gmail.com>
2021-10-24 11:56:25 +0300

    fixed int/string exceptions

--- basic_socket_ex_2.6/protocol.py
+++ basic_socket_ex_2.6/protocol.py
@@ -31,24 +31,25 @@ def __exit_response():
 COMMAND_RESPONSE = {'RAND': __rand_response, 'WHORU': __whoru_response, 'TIME': __time_response, 'EXIT': __exit_response}
 
 
-def check_cmd(data):
+def check_cmd(cmd_data):
     """Check if the command is defined in the protocol (e.g RAND, NAME, TIME, EXIT)"""
-    return data in COMMANDS
+    return cmd_data in COMMANDS
 
 
-def create_msg(data):
+def create_msg(msg_data):
     """Create a valid protocol message, with length field"""
-    msg_length = str(len(data)).zfill(LENGTH_FIELD_SIZE)
-    msg = msg_length + data
+    msg_length = str(len(msg_data)).zfill(LENGTH_FIELD_SIZE)
+    msg = msg_length + msg_data
     return msg.encode()
 
 
 def get_msg(my_socket):
     """Extract message from protocol, without the length field
        If length field does not include a number, returns False, "Error" """
-    msg_length = str(my_socket.recieve(LENGTH_FIELD_SIZE).decode())
+    msg_length = my_socket.recv(LENGTH_FIELD_SIZE).decode()
     if msg_length.isdecimal():
-        return True, str(my_socket.recieve(msg_length).decode())
+        msg_length = int(msg_length)
+        return True, str(my_socket.recv(msg_length).decode())
     else:
         return False, "Error: length field did not contain only numbers"
 
@@ -56,5 +57,5 @@ def get_msg(my_socket):
 def create_server_rsp(cmd):
     """Based on the command, create a proper response"""
     if check_cmd(cmd):
-        return COMMAND_RESPONSE[cmd]()
+        return str(COMMAND_RESPONSE[cmd]())
     return "Server response"
